a117a413aff45e5b74b80edb8131cee7b986bb30
anastasiykakhudyakova <nastya200504271@gmail.com>
2024-09-21 11:10:33 +0300

    Create Header.h

---
+++ lab2/Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef MYFUNCTION_H
+#define MYFUNCTION_H
+
+#define SUM(a, b) ((a) + (b))
+
+namespace MyNamespace {
+    using namespace std;
+    int myFunction(int num);
+}
+
+#endif 
