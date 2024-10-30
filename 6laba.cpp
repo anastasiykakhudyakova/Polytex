13e85d2bf9d1c356331753f5e7d164ea13fa606f
anastasiykakhudyakova <nastya200504271@gmail.com>
2024-10-30 09:35:53 +0300

    Update 6laba.cpp

--- 6laba.cpp
+++ 6laba.cpp
@@ -17,70 +17,90 @@
 /*/
 
 #include <iostream>
-using namespace std;
-int main() {
-    setlocale(LC_ALL, "Russian");
-    int choice;
+#include <string>
+#include <stdexcept>
+#include <ctime>
+#include <locale>
 
-    cout << "Введите число 1, 2 или 3" << endl;
-    cin >> choice;
-    try {
-        switch (choice) {
-        case 1:
-            cout << "Один" << endl;
-            break;
-        case 2:
-            cout << "Два" << endl;
-            break;
-        case 3:
-            cout << "Три" << endl;
-            break;
+// Определим собственное исключение с перечислением кодов ошибок
+class BirthYearException : public std::exception {
+public:
+    enum ErrorCode {
+        YEAR_TOO_OLD,
+        YEAR_IN_FUTURE,
+        NEGATIVE_YEAR
+    };
+
+    BirthYearException(ErrorCode code) : code(code) {}
+
+    const char* what() const noexcept override {
+        switch (code) {
+        case YEAR_TOO_OLD:
+            return "Что-то не верится...";
+        case YEAR_IN_FUTURE:
+            return "Вы ещё не родились";
+        case NEGATIVE_YEAR:
+            return "Вы ввели отрицательное число!";
         default:
-            throw runtime_error("Некорректное значение");
+            return "Неизвестная ошибка";
         }
     }
-    catch (const runtime_error& e) {
-        cout << e.what() << endl;
-        return 1;
-    }
-    int birthYear;
-    cout << "Введите год вашего рождения: ";
-    cin >> birthYear;
-    try {
-        if (birthYear < 0) {
-            throw runtime_error("Вы ввели отрицательное число");
-        }
-        else if (birthYear < 1850) {
-            throw runtime_error("Что то не верится");
-        }
-        else if (birthYear > 2024) {
-            throw runtime_error("Вы еще не родились");
-        }
 
-        cout << "Вы ввели год рождения: " << birthYear << endl;
+private:
+    ErrorCode code;
+};
 
-
+// Функция для преобразования числа в текст
+std::string numberToText(int number) {
+    switch (number) {
+    case 1: return "Один";
+    case 2: return "Два";
+    case 3: return "Три";
+    default: throw std::invalid_argument("Некорректное значение");
     }
-    catch (const runtime_error& e) {
-        cout << e.what() << endl;
-        return 1;
-    }
-    cout << "Спасибо!" << endl;
-    return 0;
 }
 
+// Функция для проверки года рождения
+void checkBirthYear(int year) {
+    int currentYear = std::time(0) / (60 * 60 * 24 * 365.25) + 1970; // получение текущего года
 
-enum ErrorCode {
-    NegativeYear,
-    FutureYear,
-    YearTooOld
-};
+    if (year < 1850) {
+        throw BirthYearException(BirthYearException::YEAR_TOO_OLD);
+    }
+    else if (year > currentYear) {
+        throw BirthYearException(BirthYearException::YEAR_IN_FUTURE);
+    }
+    else if (year < 0) {
+        throw BirthYearException(BirthYearException::NEGATIVE_YEAR);
+    }
+    else {
+        std::cout << "Спасибо!" << std::endl;
+    }
+}
 
-class BirthYearException : public runtime_error {
-public:
-    ErrorCode code;
+int main() {
+    // Устанавливаем локаль для поддержки кириллицы
+    setlocale(LC_ALL, "Russian");
 
-    BirthYearException(const string& message, ErrorCode errCode)
-        : runtime_error(message), code(errCode) {}
-};
+    try {
+        std::cout << "Введите число 1, 2 или 3: ";
+        int num;
+        std::cin >> num;
+        std::cout << numberToText(num) << std::endl;
+    }
+    catch (const std::invalid_argument& e) {
+        std::cout << e.what() << std::endl;
+    }
 
+    try {
+        std::cout << "Введите год своего рождения: ";
+        int birthYear;
+        std::cin >> birthYear;
+        checkBirthYear(birthYear);
+    }
+    catch (const BirthYearException& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+}
